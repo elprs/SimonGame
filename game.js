5655415a63c644b789314830aa00340d7e22679c
elprs <eleni.parisi.sunshine@gmail.com>
2020-04-26 13:21:16 +0300

    buttonColours, randomChosenColour

--- game.js
+++ game.js
@@ -1,1 +1,16 @@
-alert("yes")
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+
+function nextSequence(){
+   var randomNumber = Math.random();
+   randomNumber = randomNumber * 4;
+   randomNumber = Math.floor(randomNumber);
+    return randomNumber;
+
+}
+
+var randomChosenColour = buttonColours[nextSequence()];
+gamePattern.push(randomChosenColour);
+
+
