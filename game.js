3c634f4c7e8c5cbd8fb55eedf42e96c2880903b6
elprs <eleni.parisi.sunshine@gmail.com>
2020-04-26 13:56:26 +0300

    Flash and play sounds.

--- game.js
+++ game.js
@@ -1,16 +1,19 @@
 
 var buttonColours = ["red", "blue", "green", "yellow"];
+
 var gamePattern = [];
 
-function nextSequence(){
-   var randomNumber = Math.random();
-   randomNumber = randomNumber * 4;
-   randomNumber = Math.floor(randomNumber);
-    return randomNumber;
+function nextSequence() {
 
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  //1. Select the button with the same id as the randomChosenColour.
+  //2. Animate a flash to the button selected .
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  //3.  Play the sound for the button colour selected.
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
 }
-
-var randomChosenColour = buttonColours[nextSequence()];
-gamePattern.push(randomChosenColour);
-
-
