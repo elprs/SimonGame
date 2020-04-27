51bfd4061031333f242ba5cd5b16c7f24ddf8496
elprs <eleni.parisi.sunshine@gmail.com>
2020-04-27 15:23:04 +0300

    game-starts-game-completed!

--- game.js
+++ game.js
@@ -1,58 +1,79 @@
+
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
-
 var userClickedPattern = [];
 
-// 1. Start the game
+var started = false;
+var level = 0;
 
-
-//1. Add the pressed class to the button that gets clicked and flash.
-
-function animatePress(currentColour) {
-    $("#" + currentColour).addClass("pressed");
-  
-    setTimeout(function () {
-      $("#" + currentColour).removeClass("pressed");
-    }, 80);
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
   }
+});
 
-  
-//1. Play the sound for the button colour selected.
+$(".btn").click(function() {
+
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length-1);
+});
+
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+      if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+    } else {
+      playSound("wrong");
+      $("body").addClass("game-over");
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+
+      startOver();
+    }
+}
+
+
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
 function playSound(name) {
   var audio = new Audio("SimonSounds/" + name + ".mp3");
   audio.play();
 }
 
-//1. Detect when any of the buttons are clicked and trigger a handler function.
-$(".btn").click(function () {
-  //2. Inside the handler, create a new variable called userChosenColour to store the id
-  var userChosenColour = $(this).attr("id");
-  //3. Add the contents of the variable userChosenColour created in step 2 to the end of this new userClickedPattern
-  userClickedPattern.push(userChosenColour);
-
-  // Test
-  // console.log(userClickedPattern);
-
-  //4.  Play the sound for the button colour selected.
-  playSound(userChosenColour);
-
-  animatePress(userChosenColour);
-});
-
-function nextSequence() {
-  var randomNumber = Math.floor(Math.random() * 4);
-  var randomChosenColour = buttonColours[randomNumber];
-  gamePattern.push(randomChosenColour);
-
-  //1. Select the button with the same id as the randomChosenColour.
-  //2. Animate a flash to the button selected .
-  $("#" + randomChosenColour)
-    .fadeIn(100)
-    .fadeOut(100)
-    .fadeIn(100);
-
-  //3.  Play the sound for the button colour selected.
-  playSound(randomChosenColour);
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
 }
-
